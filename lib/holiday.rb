a5d942f1dc9327285f8728b652cd9b739d6fc943
Ashna Shah-Grover <as19@williams.edu>
2019-05-17 22:15:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,10 +65,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  #holiday_hash.each do |season, holidays|
-    #puts "#{season.to_s.capitalize}:"
-    #holidays.each do |holiday, supplies|
-      #puts 
+  holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts "#{holiday}:#{supplies.join(", ")}"
 
 end
 
