ba25bf1d33006aeb515717aa12df5536892436a3
Ashna Shah-Grover <as19@williams.edu>
2019-05-17 22:18:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season, holidays|
+    season.to_s
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
       puts "#{holiday}:#{supplies.join(", ")}"
