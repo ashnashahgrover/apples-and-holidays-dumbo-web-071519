3017ca3194f14b51445b746f8dddfb05fa325784
Ashna Shah-Grover <as19@williams.edu>
2019-05-17 21:59:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,11 +75,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  bbq = []
   holiday_hash.each do |season, holidays|
     holiday_hash.each do |holiday, supplies|
-      if 
-
+      if supplies.include?("BBQ")
+        bbq.push(holiday)
+      end 
+    end 
+  end 
+  bbq 
 end
 
 
